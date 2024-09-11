d60a29fe850a10fff07b5e5a979b714941e1f613
Sattibabu718 <padalasattibabu24@gmail.com>
2024-09-11 15:40:18 +0530

    init

---
+++ TS/index.js
@@ -0,0 +1,4 @@
+var x = 25;
+console.log(x, typeof x);
+var p = "sattibabu";
+console.log(p, typeof p);
---
+++ TS/index.ts
@@ -0,0 +1,4 @@
+let x = 25
+console.log(x, typeof x);
+let p = "sattibabu"
+console.log(p, typeof p);
