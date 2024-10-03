82d81d8b605bc38ee42cc9967a919535c44965ae
Sattibabu718 <padalasattibabu24@gmail.com>
2024-10-03 19:15:35 +0530

    init

--- Angular/projects/randomQuote/src/app/app.component.spec.ts
+++ Angular/projects/randomQuote/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'second'`, () => {
+  it(`should have as title 'randomQuote'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('second');
+    expect(app.title).toEqual('randomQuote');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, second');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, randomQuote');
   });
 });
